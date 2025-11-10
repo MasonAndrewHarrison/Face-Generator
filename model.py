ed36e19837e7ccd8d10efeeca6d6dcb50573d26b
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2025-11-10 03:24:35 -0500

    second commit

--- model.py
+++ model.py
@@ -6,40 +6,50 @@ class Discriminator(nn.Module):
     def __init__(self, img_dim, features):
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(
-            nn.Conv2d(1, features, 5, 1, 2),
-            nn.ReLU(inplace=True),
-
-            self._block(features, features*2, 3, 2, 1),
-            self._block(features*2, features*4, 3, 2, 1),
+            
+            self._Depthwise_Separable_Conv2d(3, features, 5, 2, 2),
+            self._Depthwise_Separable_Conv2d(features, features*2, 3, 2, 1),
             nn.Dropout(0.25),
 
-            self._block(features*4, features*8, 3, 2, 1),
-            self._block(features*8, features*16, 3, 2, 1),
+            self._Depthwise_Separable_Conv2d(features*2, features*4, 3, 2, 1),
+            self._Depthwise_Separable_Conv2d(features*4, features*8, 3, 2, 1),
             nn.Dropout(0.25),
 
-            self._block(features*16, features*32, 3, 2, 1),
-
-            nn.Conv2d(features*32, 1, 2, 1, 0),
-            nn.ReLU(inplace=True),
+            self._Depthwise_Separable_Conv2d(features*8, features*16, 3, 2, 1),
+            
+            self._Depthwise_Separable_Conv2d(features*16, 1, 2, 1, 0, activation=False),
             nn.Sigmoid()
         )
     
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
+    def _Depthwise_Separable_Conv2d(self, in_channels, out_channels, kernel_size, stride, padding, activation=True):
+        layers = [
             nn.Conv2d(
                 in_channels=in_channels,
-                out_channels=out_channels,
+                out_channels=in_channels,
                 kernel_size=kernel_size,
                 stride=stride,
                 padding=padding,
-                bias=False
+                bias=not activation,
+                groups=in_channels
             ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                bias=not activation,               
+            ),
+        ]
+
+        if activation:
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+        
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        return self.disc(x)
+        return self.disc(x).view(-1, 1)
 
 
 
@@ -47,7 +57,7 @@ if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    image = torch.randn(1, 1, 64, 64).to(device)
+    image = torch.randn(1, 3, 64, 64).to(device)
     disc = Discriminator(64, 64).to(device)
 
     print(image.shape)
