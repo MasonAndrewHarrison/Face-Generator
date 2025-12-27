0bfb553d6cdeec58f3fbb8ff6aab1d6410a6584d
mason <you@example.com>
2025-12-26 19:37:00 -0500

    updates

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 z_dim = 100
 gen_features = 32
-
+batch_size = 3
 
 gen_model = Generator(z_dim, gen_features).to(device)
 critic_model = Critic(gen_features).to(device)
@@ -25,20 +25,17 @@ else:
     exit()
 
 
-z_noise = torch.randn(1, z_dim, 1, 1).to(device)
+z_noise = torch.randn(batch_size, z_dim, 1, 1).to(device)
 
-real = torch.randn(1, 3, 64, 64).to(device)
+real = torch.randn(batch_size, 3, 64, 64).to(device)
 fake = gen_model(z_noise)
 
-gradient_penalty = gradient_penalty(critic_model, real, fake, device=device)
-print(f"Gradient Penalty: {gradient_penalty.item():.4f}")
 
 
 gen_model.eval()
 with torch.no_grad():
     generated_image = gen_model(z_noise).detach().cpu()
     image = generated_image[0, :, :, :].permute(1, 2, 0) * 0.5 + 0.5
-    print(image)
     plt.imshow(image)
     plt.show()
 
